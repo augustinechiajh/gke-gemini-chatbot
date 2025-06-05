5b3186e1eb82dd0820280f667d6ac3090b4e00ed
augustine023 <augustine023@gmail.com>
2025-06-05 22:43:11 +0800

    Update iam.tf

--- iac/tf/iam.tf
+++ iac/tf/iam.tf
@@ -32,3 +32,9 @@ resource "google_project_iam_member" "gke_node_sa_artifacts" {
   role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
 }
+
+resource "google_project_iam_member" "gke_node_sa_artifacts" {
+  project = var.project_id
+  role    = "roles/compute.viewer"
+  member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
+}
